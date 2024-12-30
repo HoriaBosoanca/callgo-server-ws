a6dd835c25a54ed710f62814eb2ca1bd85b0b9f5
REDplayer333HHH <horia.bosoanca@gmail.com>
2024-12-30 17:41:46 +0200

    began writing ws server

---
+++ callgo/callgo.go
@@ -0,0 +1,113 @@
+package callgo
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
+	"github.com/teris-io/shortid"
+)
+
+func HandleEndpoint(router *mux.Router) {
+	router.HandleFunc("/ws", OptionsHandler).Methods("OPTIONS")
+	router.HandleFunc("/ws", handleWebSockets)
+}
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+type Member struct {
+	DisplayName string
+	ID string
+	Connection *websocket.Conn
+}
+
+type VideoData struct {
+	VideoData string `json:"video"`
+}
+
+var sessions map[string]map[string]Member = make(map[string]map[string]Member)
+
+// /ws?sessionID=abcd
+func handleWebSockets(w http.ResponseWriter, r *http.Request) {
+	connection, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer connection.Close()
+
+	// Initialize member
+	sessionID := r.URL.Query().Get("sessionID")
+	if sessionID == "" {
+		http.Error(w, "Wrong session ID", http.StatusBadRequest)
+		return
+	}
+	displayName := r.URL.Query().Get("displayName")
+	if displayName == "" {
+		http.Error(w, "Error with display name", http.StatusBadRequest)
+		return
+	}
+	memberID, err := addMember(sessionID, displayName, connection)
+	if err != nil {
+		log.Println("Error adding member:", err)
+		return
+	}
+
+	for {
+		// RECEIVE
+		var receivedVideo VideoData
+		err := connection.ReadJSON(&receivedVideo)
+		if err != nil {
+			log.Println("Error reading message:", err)
+			break
+		}
+
+		// SEND
+		for _, member := range sessions[sessionID] {
+			err = member.Connection.WriteJSON(receivedVideo)
+			if err != nil {
+				log.Println("Error writing message:", err)
+				break
+			}
+		}
+	}
+
+	disconnect(sessionID, memberID)
+}
+
+func addMember(sessionID string, displayName string, connection *websocket.Conn) (memberID string, err error) {
+	session, exists := sessions[sessionID]
+	if !exists {
+		session = make(map[string]Member)
+	}
+	memberID, err = shortid.Generate()
+	session[memberID] = Member{DisplayName: displayName, Connection: connection, ID: memberID}
+	sessions[sessionID] = session 
+	return memberID, err
+}
+
+func disconnect(sessionID string, memberID string) {
+	session, exists := sessions[sessionID]
+	if !exists {
+		log.Println("Session doesn't exist:", sessionID, memberID)
+		return
+	}
+	member, exists2 := session[memberID]
+	if !exists2 {
+		log.Println("Member doesn't exist:", sessionID, memberID)
+		return
+	}
+
+	member.Connection.Close()
+	delete(session, memberID)
+	sessions[sessionID] = session
+
+	if len(session) == 0 {
+		delete(sessions, sessionID)
+	}
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"os"
+	"net/http"
+
+	"ws/callgo"
+	
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	// init
+	router := mux.NewRouter()
+	router.Use(callgo.EnableCORS)
+	
+	// get port
+	port := os.Getenv("PORT")
+    if port == "" {
+        port = "1234"
+    }
+
+	// Handle endpoints
+	callgo.HandleEndpoint(router)
+
+	// log and start
+	log.Printf("Server starting on port %s.", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
